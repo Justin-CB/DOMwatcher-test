17ad5907a2f298424c34632a6922bafb995381b0
Justin-CB <animalofthegalaxy@gmail.com>
2020-03-24 23:15:30 -0400

    Fixed CSS Animation Detection

--- DOMwatcher.js
+++ DOMwatcher.js
@@ -6,19 +6,16 @@ function dynamic_content_listener(event) {
     num_animations_observed++;
 }
 
-function polyfill_dynamic_content()
-{
-    var style=document.createElement("style");
-    var sheet;
-    style.appendChild(document.createTextNode(""));
-    document.head.appendChild(style);
-    sheet=style.sheet;
-    sheet.insertRule("@keyframes ruffleObjectOrEmbedInserted { from { opacity: 0.99; } to { opacity: 1; } }",-1);
-    sheet.insertRule("object, embed { animation-duration:0.001s; animation-name: ruffleObjectOrEmbedInserted; }",-1);
-    document.addEventListener("animationstart", dynamic_content_listener, false);
-    document.addEventListener("MSAnimationStart", dynamic_content_listener, false);
-    document.addEventListener("webkitAnimationStart", dynamic_content_listener, false);
-}
+var style=document.createElement("style");
+var sheet;
+style.appendChild(document.createTextNode(""));
+document.head.appendChild(style);
+sheet=style.sheet;
+sheet.insertRule("@keyframes ruffleObjectOrEmbedInserted { from { opacity: 0.99; } to { opacity: 1; } }",0);
+sheet.insertRule("object, embed { animation-duration:0.001s; animation-name: ruffleObjectOrEmbedInserted; }",1);
+document.addEventListener("animationstart", dynamic_content_listener, false);
+document.addEventListener("MSAnimationStart", dynamic_content_listener, false);
+document.addEventListener("webkitAnimationStart", dynamic_content_listener, false);
 function log_variables()
 {
     console.log("# of mutations: "+num_mutations_observed+" | # of animations: "+num_animations_observed);
